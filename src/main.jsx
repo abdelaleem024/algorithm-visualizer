9ebe6eacb5c8bb64d5e1414f9785fcaaee4166b2
abdelaleem-abdelaleem <abdelaleem.abdelaleem@bld.ai>
2024-01-19 14:23:28 +0200

    test: try to solve deploy problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/index.js";
 import "intro.js/introjs.css";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+// import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: ,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
