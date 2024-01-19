3f26a658456f7b46d5dfe02c3a210fdd7cc59760
abdelaleem-abdelaleem <abdelaleem.abdelaleem@bld.ai>
2024-01-19 12:49:28 +0200

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/index.js";
 import "intro.js/introjs.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
